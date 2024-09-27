0e779b9032774b7b43731ff6c05d099d845ba95f
박건우 <136959177+chaos1231107@users.noreply.github.com>
2024-09-28 08:46:32 +0900

    selection sort(선택정렬)

---
+++ selection_sort.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void selection_sort(int list[], int n)
+{
+    int least, temp;
+    for (int i = 0; i < n-1; i++)
+    {
+        least = i;
+        for (int j = i+1; j < n; j++)
+        {
+            if (list[least] > list[j])
+            {
+                least = j;
+            }
+        }
+        //temp = list[i];
+        //list[i] = list[least];
+        //list[least] = temp;
+        swap(list[i], list[least]);
+    }
+    
+}
+int main()
+{
+    int n;
+    cin >> n; 
+    vector<int> list(n);
+    
+    for (int i = 0; i < n; i++)
+    {
+        cin >> list[i];
+    }
+    selection_sort(list.data(), n);
+    
+    for (int i = 0; i < n; i++)
+    {
+        cout << list[i] << " ";
+    }
+
+    return 0;
+}
