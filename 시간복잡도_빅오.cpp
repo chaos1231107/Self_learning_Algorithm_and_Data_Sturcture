b21c724e02a2842d5303353f85c4cd3b7cf225f5
박건우 <136959177+chaos1231107@users.noreply.github.com>
2024-09-29 09:50:53 +0900

    시간복잡도 빅O 표기법

---
+++ 시간복잡도_빅오.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+int f(int a1, int n, int a8)
+{
+    return a1*n + a8;
+}
+int g(int n)
+{
+    return n;
+}
+int main()
+{
+    int a1,a8;
+    int c;
+    int n;
+    
+    cin >> a1 >> a8;
+    cin >> c;
+    cin >> n;
+    
+    if (f(a1, n, a8) <= c*g(n) && a1 <= c)
+    {
+        cout << 1;
+    }
+    else 
+    {
+        cout << 0;
+    }
+    
+}
