c35e536ca293ade1fadde8929dd5591286a872f2
박건우 <136959177+chaos1231107@users.noreply.github.com>
2024-10-02 12:08:25 +0900

    Update binary_search.cpp

--- binary_search.cpp
+++ binary_search.cpp
@@ -14,11 +14,12 @@ bool binary_search(vector<int>& arr, int len, int target)
         {
             return true; 
         }
-        
+        // targtet이 중간값보다 작다면 범위를 더 낮게 잡아야 한다
         if (target < arr[mid])
         {
             high = mid - 1;
         }
+        // target이 중간값보다 크다면 범위를 더 크게 잡아야 한다
         else if (target > arr[mid])
         {
             low = mid + 1;
