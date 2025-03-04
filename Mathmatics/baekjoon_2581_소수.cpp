b0f2168dcc25166e25f05a9d30859d3e74b86c00
Keonwoo Park <136959177+chaos1231107@users.noreply.github.com>
2025-03-05 07:24:54 +0900

    Update baekjoon_2581_소수.cpp

--- Mathmatics/baekjoon_2581_소수.cpp
+++ Mathmatics/baekjoon_2581_소수.cpp
@@ -1,3 +1,63 @@
+// ver1 : C++ 17
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <climits>
+
+using namespace std;
+
+vector<int> sieve(int n, int a, int b) 
+{
+    vector<bool> prime(n + 1, true);
+    prime[0] = prime[1] = false;
+    
+    for (int p = 2; p * p <= n; p++) 
+    {
+        if (prime[p]) 
+        {
+            for (int i = 2 * p; i <= n; i += p) 
+                prime[i] = false;
+        }
+    }
+    
+    vector<int> primes;
+    for (int p = a; p <= b; p++) 
+    {
+        if (prime[p]) 
+            primes.push_back(p);
+    }
+
+    return primes;  // Return the prime numbers found in the range [a, b]
+}
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int a, b;
+    cin >> a >> b;
+
+    vector<int> primes = sieve(b, a, b);
+    int sum = 0;
+    int minprime = INT_MAX;
+
+    for (int i = 0; i < primes.size(); i++)
+    {
+        sum += primes[i];
+        if (primes[i] < minprime)
+            minprime = primes[i];
+    }
+
+    if (primes.size() == 0) 
+        cout << -1 << endl;
+    else 
+        cout << sum << endl << minprime << endl;
+
+    return 0;
+}
+// ver2 : modern C++
 #include <iostream>
 #include <vector>
 #include <cmath>
