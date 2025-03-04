f15c5214d9d86b2847376a6c32cffbb410ebe673
박건우 <136959177+chaos1231107@users.noreply.github.com>
2025-03-04 13:19:09 +0900

    Update baekjoon_1065_한수.cpp

--- Mathmatics/baekjoon_1065_한수.cpp
+++ Mathmatics/baekjoon_1065_한수.cpp
@@ -1,3 +1,4 @@
+// c++17 ver
 #include <bits/stdc++.h>
 using namespace std;
 //static int cnt = 0;
@@ -41,5 +42,57 @@ int main()
  
     
     cout << cnt << endl;
+    return 0;
+}
+
+//C99 ver
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+bool is_arith_seq(int number)
+{
+    int digits[10];
+    int len = 0;
+    while (number > 0)
+    {
+        digits[len++] = number % 10;
+        number /= 10;
+    }
+    
+    for (int i = 0; i < len/2; i++)
+    {
+        int temp = digits[i];
+        digits[i] = digits[len - i -1];
+        digits[len - i - 1] = temp;
+    }
+    
+    if (len <= 2) 
+        return true;
+    
+    int difference = digits[1] - digits[0];
+    for (int i = 2; i < len; i++)
+    {
+        if (digits[i] - digits[i-1] != difference)
+            return false;
+    }
+    
+    return true;
+}
+int main()
+{
+    int number;
+    int cnt = 0;
+    //int arr[1001];
+    //int len = sizeof(arr) / sizeof(int);
+    scanf("%d", &number);
+    
+    for (int i = 1; i <= number; i++)
+    {
+        if (is_arith_seq(i))
+            cnt++;
+    }
+    
+    printf("%d", cnt);
     return 0;
 }
