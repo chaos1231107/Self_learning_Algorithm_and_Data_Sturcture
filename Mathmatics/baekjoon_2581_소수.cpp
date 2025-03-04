4f6a817eae1f1b79d0970ffe8dfe35047b651eb7
Keonwoo Park <136959177+chaos1231107@users.noreply.github.com>
2025-03-04 17:21:45 +0900

    Create baekjoon_2581_소수.cpp

---
+++ Mathmatics/baekjoon_2581_소수.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <climits>
+
+using namespace std;
+
+vector<int> sieve(int n, int a, int b) 
+{
+    vector<bool> prime(n + 1, true);
+    prime[0] = prime[1] = false;
+    
+    for (int p = 2; p * p <= n; p++) 
+    {
+        if (prime[p]) 
+        {
+            for (int i = p * p; i <= n; i += p) 
+                prime[i] = false;
+        }
+    }
+    
+    vector<int> primes;
+    for (int p = a; p <= b; p++) 
+    {
+        if (prime[p]) 
+            primes.push_back(p);
+    }
+
+    return primes;  // Return the prime numbers found in the range [a, b]
+}
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int a, b;
+    cin >> a >> b;
+
+    vector<int> primes = sieve(b, a, b);
+    int sum = 0;
+    int minprime = INT_MAX;
+
+    for (int prime : primes) 
+    {
+        sum += prime;
+        minprime = min(minprime, prime);
+    }
+
+    if (primes.empty()) 
+        cout << -1 << endl;
+    else 
+        cout << sum << endl << minprime << endl;
+
+    return 0;
+}
