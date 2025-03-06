c1d817e5e5532afcffd5e68b687091ba6c8811f6
Keonwoo Park <136959177+chaos1231107@users.noreply.github.com>
2025-03-06 13:38:39 +0900

    Create boj_4948_베르트랑공준.py

---
+++ Mathmatics/boj_4948_베르트랑공준.py
@@ -0,0 +1,23 @@
+import math
+
+def is_prime(n):
+    if n == 1:
+        return False
+    for i in range(2, int(math.sqrt(n)+1)):
+        if n % i == 0:
+            return False
+    return True
+l = []
+
+while True:
+    n = int(input())
+    #l.append(n)
+    if n == 0:
+        break
+    cnt = 0
+    for p in range(n+1, 2*n+1):
+        if is_prime(p):
+            cnt+=1
+    
+    
+    print(cnt)
