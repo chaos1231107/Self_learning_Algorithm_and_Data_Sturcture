b2aff597c899d9ee5e048a0ab7b73fa978b2bd5f
Keonwoo Park <136959177+chaos1231107@users.noreply.github.com>
2025-03-11 16:23:10 +0900

    Create boj_23827_prefix_sum.cpp

---
+++ Mathmatics/prefix_sum/boj_23827_prefix_sum.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define MOD 1000000007
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    long long int N; 
+    cin >> N;
+    vector<long long int> A(N);
+    long long int total_sum = 0;
+    long long int prefix_sum = 0;
+    
+    for (long long int i = 0; i < N; i++)
+        cin >> A[i];
+    
+    
+    for (long long int i = 0; i < N; i++)
+        total_sum += A[i];
+        
+    for (long long int i = 0; i < N; i++)
+    {
+        total_sum -= A[i];
+        prefix_sum += A[i] * total_sum;
+        prefix_sum %= MOD; //오버플로우 방지
+    }
+    
+    if (prefix_sum < 0)
+        prefix_sum += MOD;
+        
+    cout << prefix_sum % MOD << endl;
+    return 0;
+}
