ed0b695d1bf3b6438aec2ed5735d4ce9a6a8c73f
Keonwoo Park <136959177+chaos1231107@users.noreply.github.com>
2025-05-19 12:58:26 +0900

    Update 2d_prefix.py

--- prefix_sum/2d_prefix.py
+++ prefix_sum/2d_prefix.py
@@ -1,19 +1,22 @@
 import sys
+input = sys.stdin.readline
 
-N, M = map(int, sys.stdin.readline().split())
-data = []
+N, M = map(int, input().split())
+arr = [] 
+
 for _ in range(N):
-    data.append(list(map(int, sys.stdin.readline().split())))
+    arr.append(list(map(int, input().split())))
     
-total = [[0] * (N+1) for _ in range(N+1)]
+prefix_sum = [[0] * (N+1) for _ in range(N+1)]
 
 for y in range(1, N+1):
     for x in range(1, N+1):
-        total[y][x] = data[y-1][x-1] + total[y-1][x] \
-        + total[y][x-1] - total[y-1][x-1]
-                    
+        prefix_sum[y][x] = arr[y-1][x-1] + prefix_sum[y-1][x] \
+                            + prefix_sum[y][x-1] - prefix_sum[y-1][x-1]
+
 for _ in range(M):
-    y1, x1, y2, x2 = map(int, sys.stdin.readline().split())
-    result = total[y2][x2] - total[y1-1][x2] - total[y2][x1-1] \
-            + total[y1-1][x1-1]
+    y1, x1, y2, x2 = map(int, input().split())
+    result = prefix_sum[y2][x2] - prefix_sum[y1-1][x2] \
+                - prefix_sum[y2][x1-1] + prefix_sum[y1-1][x1-1]
+            
     print(result)
