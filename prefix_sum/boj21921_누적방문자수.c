5beaf000fec8d0a31ac94684aeb4765ec366a74a
Keonwoo Park <136959177+chaos1231107@users.noreply.github.com>
2025-05-20 11:48:12 +0900

    Create boj21921_누적방문자수.c

---
+++ prefix_sum/boj21921_누적방문자수.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int N, X;
+    scanf("%d %d", &N, &X);
+    int *arr = calloc(N+1, sizeof(int));
+    int *prefix_sum = calloc(N+2, sizeof(int));
+    
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    
+    for (int i = 0; i < N+1; i++)
+    {
+        prefix_sum[i+1] = prefix_sum[i] + arr[i];
+    }
+    
+    int cnt = 0;
+    int max_sum = 0;
+    
+    for (int i = 0; i < N-X+1; i++)
+    {
+        int current_sum = prefix_sum[i+X] - prefix_sum[i];
+        if (current_sum > max_sum)
+        {
+            max_sum = current_sum;
+            cnt = 1;
+        }
+        else if (current_sum == max_sum)
+        {
+            cnt++;
+        }
+    }
+    
+    if (max_sum == 0)
+        printf("SAD\n");
+    else
+    {
+        printf("%d\n", max_sum);
+        printf("%d", cnt);
+    }
+    
+    free(arr);
+    free(prefix_sum);
+    return 0;
+}
